93016e5285fb7310bb4511bc721fc0610f0edae4
Gracen hudachek <gracenhudachek@Gracens-MBP.attlocal.net>
2021-06-16 08:43:18 -0500

    mmerging

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-
 import { Link, Route, Switch } from "react-router-dom";
 import HomePage from "./components/HomePage/HomePage";
 import TaskForm from "./components/TaskForm/TaskForm";
 import Category from "./components/CategoryPage/Category";
-import Details from "./components/DetailsPage/Details";
-import Footer from './components/Footer/Footer';
-
+import Footer from "./components/Footer/Footer";
+import Schedule from "./components/Schedule/Schedule";
 function App() {
   return (
     <div className="App">
@@ -26,8 +24,8 @@ function App() {
         <Route exact path="/caretaking">
           <Category category="caretaking" />
         </Route>
-        <Route exact path="/details">
-          <Details />
+        <Route exact path="/schedule">
+          <Schedule />
         </Route>
       </Switch>
       <Footer />
