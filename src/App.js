8a9e1ae717de036d22ecb27694398c3c4c886fb4
Gracen hudachek <gracenhudachek@Gracens-MBP.attlocal.net>
2021-06-16 11:16:28 -0500

    merging

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Register from "./components/Register/Register";
 import HomePage from "./components/HomePage/HomePage";
 import TaskForm from "./components/TaskForm/TaskForm";
 import Category from "./components/CategoryPage/Category";
-import Details from "./components/DetailsPage/Details";
 import Footer from "./components/Footer/Footer";
 import VolunteerModal from "./components/VolunteerModal/VolunteerModal";
 
@@ -16,10 +15,10 @@ function App() {
         <Route path="/home">
           <HomePage />
         </Route>
-        <Route path="/">
+        <Route exact path="/">
           <Login />
         </Route>
-        <Route path="/register">
+        <Route exact path="/register">
           <Register />
         </Route>
         <Route exact path="/add">
@@ -30,9 +29,6 @@ function App() {
         </Route>
         <Route exact path="/caretaking">
           <Category category="caretaking" />
-        </Route>
-        <Route exact path="/details">
-          <Details />
         </Route>
       </Switch>
       <Footer />
