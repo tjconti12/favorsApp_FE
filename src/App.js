2c1bd4360999a564ef61c2112d8f8b258938d466
ryfoa6 <ryfo720@gmail.com>
2021-06-16 12:14:00 -0700

    Pass loggedIn prop to each component

--- src/App.js
+++ src/App.js
@@ -1,62 +1,60 @@
-import { useState } from 'react';
-import { Route, Switch } from "react-router-dom";
-import Login from "./components/Login/Login";
-import Register from "./components/Register/Register";
-import HomePage from "./components/HomePage/HomePage";
-import TaskForm from "./components/TaskForm/TaskForm";
-import Category from "./components/CategoryPage/Category";
-import Footer from "./components/Footer/Footer";
-import VolunteerModal from "./components/VolunteerModal/VolunteerModal";
-import Profile from "./components/Profile/Profile";
-import Schedule from "./components/Schedule/Schedule";
-
+import { useState } from 'react'
+import { Route, Switch } from 'react-router-dom'
+import Login from './components/Login/Login'
+import Register from './components/Register/Register'
+import HomePage from './components/HomePage/HomePage'
+import TaskForm from './components/TaskForm/TaskForm'
+import Category from './components/CategoryPage/Category'
+import Footer from './components/Footer/Footer'
+import VolunteerModal from './components/VolunteerModal/VolunteerModal'
+import Profile from './components/Profile/Profile'
+import Schedule from './components/Schedule/Schedule'
 
 function App() {
-  const [addModalOpen, setAddModalOpen] = useState(false);
+	const [loggedIn, setLoggedIn] = useState(false)
+	const [addModalOpen, setAddModalOpen] = useState(false)
 
-
-
-  return (
-    <div className="App">
-      <Switch>
-        <Route exact path="/home">
-          <HomePage setAddModalOpen={setAddModalOpen} />
-        </Route>
-        <Route exact path="/">
-          <Login />
-        </Route>
-        <Route path="/register">
-          <Register />
-        </Route>
-        {/* <Route exact path="/add">
+	return (
+		<div className='App'>
+			<Switch>
+				<Route exact path='/home'>
+					<HomePage setAddModalOpen={setAddModalOpen} />
+				</Route>
+				<Route exact path='/'>
+					<Login setLoggedIn={setLoggedIn} />
+				</Route>
+				<Route path='/register'>
+					<Register />
+				</Route>
+				{/* <Route exact path="/add">
           <TaskForm />
         </Route> */}
-        <Route exact path="/shopping">
-          <Category category="shopping" />
-        </Route>
-        <Route exact path="/caretaking">
-          <Category category="caretaking" />
-        </Route>
-        <Route exact path="/cleaning">
-          <Category category="cleaning" />
-        </Route>
-        <Route exact path="/transportation">
-          <Category category="transportation" />
-        </Route>
-        <Route exact path="/profile">
-          <Profile />
-        </Route>
-        <Route exact path="/schedule">
-          <Schedule />
-        </Route>
-        {/* <Route exact path="/details">
+				<Route exact path='/shopping'>
+					<Category loggedIn={loggedIn} category='shopping' />
+				</Route>
+				<Route exact path='/caretaking'>
+					<Category loggedIn={loggedIn} category='caretaking' />
+				</Route>
+				<Route exact path='/cleaning'>
+					<Category loggedIn={loggedIn} category='cleaning' />
+				</Route>
+				<Route exact path='/transportation'>
+					<Category loggedIn={loggedIn} category='transportation' />
+				</Route>
+				<Route exact path='/profile'>
+					<Profile loggedIn={loggedIn} setLoggedIn={setLoggedIn} />
+				</Route>
+				<Route exact path='/schedule'>
+					<Schedule loggedIn={loggedIn} />
+				</Route>
+				{/* <Route exact path="/details">
           <Details />
         </Route> */}
-      </Switch>
-      <Footer setAddModalOpen={setAddModalOpen}/>
-      {addModalOpen ? <TaskForm setAddModalOpen={setAddModalOpen}/> : <></>}
-    </div>
-  );
+			</Switch>
+			<Footer setAddModalOpen={setAddModalOpen} />
+			{addModalOpen ? <TaskForm setAddModalOpen={setAddModalOpen} /> : <></>}
+		</div>
+	)
 }
 
-export default App;
+export default App
