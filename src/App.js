8f07938e71d8bb00fddfc331b47273a0999b8838
tjconti12 <tjconti12@gmail.com>
2021-06-16 01:24:49 -0400

    create volunteer modal

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import TaskForm from "./components/TaskForm/TaskForm";
 import Category from "./components/CategoryPage/Category";
 import Details from "./components/DetailsPage/Details";
 import Footer from './components/Footer/Footer';
+import VolunteerModal from "./components/VolunteerModal/VolunteerModal";
 
 function App() {
   return (
@@ -30,6 +31,7 @@ function App() {
           <Details />
         </Route>
       </Switch>
+      <VolunteerModal />
       <Footer />
     </div>
   );
