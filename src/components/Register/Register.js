6863e197d014bbda3ee1e29d037ce4675a26cff2
Gracen hudachek <gracenhudachek@Gracens-MBP.attlocal.net>
2021-06-16 14:27:56 -0500

    fixing register page

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,10 +1,29 @@
 import { useState } from "react";
-import { Link, Redirect } from "react-router-dom";
+import { Redirect } from "react-router-dom";
 import axios from "axios";
 import ENDPOINT from "../../config/config";
+import Select from "@material-ui/core/Select";
+import InputLabel from "@material-ui/core/InputLabel";
+import { makeStyles } from "@material-ui/core/styles";
+import FormControl from "@material-ui/core/FormControl";
+import OutlinedInput from "@material-ui/core/OutlinedInput";
+import MenuItem from "@material-ui/core/MenuItem";
+import "./Register.css";
 
 const Register = () => {
   const [redirect, setRedirect] = useState(false);
+
+  const useStyles = makeStyles((theme) => ({
+    formControl: {
+      margin: theme.spacing(1),
+      minWidth: 120,
+    },
+    selectEmpty: {
+      marginTop: theme.spacing(2),
+    },
+  }));
+
+  const classes = useStyles();
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -21,7 +40,7 @@ const Register = () => {
   };
 
   if (redirect) {
-    return <Redirect to="/" />;
+    return <Redirect to="/login" />;
   }
 
   return (
@@ -31,30 +50,48 @@ const Register = () => {
       </center>
       <form onSubmit={handleSubmit}>
         <center>
-          <label htmlFor="profile-icon">Profile Icon</label>
-          <select id="profile" name="profile-icon">
-            <option value="sample1">Sample1</option>
-            <option value="sample2">Sample2</option>
-            <option value="sample3">Sample3</option>
-          </select>
+          <FormControl className={classes.formControl}>
+            <InputLabel id="demo-simple-select-label">Profile Icon</InputLabel>
+            <Select labelId="demo-simple-select-label" id="demo-simple-select">
+              <MenuItem value="sample1" className="menu-items">
+                Sample1
+              </MenuItem>
+              <MenuItem value="sample2">Sample2</MenuItem>
+              <MenuItem value="sample3">Sample 3</MenuItem>
+            </Select>
+          </FormControl>
         </center>
         <center>
-          <label htmlFor="username">Username:</label>
-          <input id="username" className="register-field" type="text" />
+          <FormControl variant="outlined">
+            <InputLabel htmlFor="username">Username</InputLabel>
+            <OutlinedInput
+              id="username"
+              className="register-field"
+              type="text"
+              label="username"
+            />
+          </FormControl>
         </center>
         <br />
         <center>
-          <label htmlFor="password">Password:</label>
-          <input id="password" className="register-field" type="password" />
+          <FormControl variant="outlined">
+            <InputLabel htmlFor="component-outlined">Password</InputLabel>
+            <OutlinedInput
+              id="component-outlined"
+              className="register-field"
+              type="password"
+              label="password"
+            />
+          </FormControl>
         </center>
+
         <center>
           <br />
-          <button type="submit">Register</button>
+          <button id="register" type="submit">
+            SIGN UP!
+          </button>
         </center>
       </form>
-      <Link to="/">
-        <button>Log In</button>
-      </Link>
     </div>
   );
 };
