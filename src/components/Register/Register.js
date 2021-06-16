08be1e55f108210520a94afb6626652f2dae46fc
Gracen hudachek <gracenhudachek@Gracens-MBP.attlocal.net>
2021-06-16 11:15:59 -0500

    add css to register

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,59 +1,99 @@
-import { useState } from 'react'
-import { Redirect } from 'react-router-dom'
-import axios from 'axios'
-import ENDPOINT from '../../config/config'
+import { useState } from "react";
+import { Redirect } from "react-router-dom";
+import axios from "axios";
+import ENDPOINT from "../../config/config";
+import Select from "@material-ui/core/Select";
+import InputLabel from "@material-ui/core/InputLabel";
+import { makeStyles } from "@material-ui/core/styles";
+import FormControl from "@material-ui/core/FormControl";
+import OutlinedInput from "@material-ui/core/OutlinedInput";
+import MenuItem from "@material-ui/core/MenuItem";
+import "./Register.css";
 
 const Register = () => {
-	const [redirect, setRedirect] = useState(false)
+  const [redirect, setRedirect] = useState(false);
 
-	const handleSubmit = (e) => {
-		e.preventDefault()
+  const useStyles = makeStyles((theme) => ({
+    formControl: {
+      margin: theme.spacing(1),
+      minWidth: 120,
+    },
+    selectEmpty: {
+      marginTop: theme.spacing(2),
+    },
+  }));
 
-		const data = {
-			profileIcon: e.target.profile.value,
-			username: e.target.username.value,
-			password: e.target.password.value,
-		}
+  const classes = useStyles();
 
-		const url = ENDPOINT + '/users/register'
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-		axios.post(url, data).then((res) => setRedirect(true))
-	}
+    const data = {
+      profileIcon: e.target.profile.value,
+      username: e.target.username.value,
+      password: e.target.password.value,
+    };
 
-	if (redirect) {
-		return <Redirect to='/login' />
-	}
+    const url = ENDPOINT + "/users/register";
 
-	return (
-		<div className='register-page'>
-			<center>
-				<h1>Sign Up!</h1>
-			</center>
-			<form onSubmit={handleSubmit}>
-				<center>
-					<label htmlFor='profile-icon'>Profile Icon</label>
-					<select id='profile' name='profile-icon'>
-						<option value='sample1'>Sample1</option>
-						<option value='sample2'>Sample2</option>
-						<option value='sample3'>Sample3</option>
-					</select>
-				</center>
-				<center>
-					<label htmlFor='username'>Username:</label>
-					<input id='username' className='register-field' type='text' />
-				</center>
-				<br />
-				<center>
-					<label htmlFor='password'>Password:</label>
-					<input id='password' className='register-field' type='password' />
-				</center>
-				<center>
-					<br />
-					<button type='submit'>Register</button>
-				</center>
-			</form>
-		</div>
-	)
-}
+    axios.post(url, data).then((res) => setRedirect(true));
+  };
 
-export default Register
+  if (redirect) {
+    return <Redirect to="/login" />;
+  }
+
+  return (
+    <div className="register-page">
+      <center>
+        <h1>Sign Up!</h1>
+      </center>
+      <form onSubmit={handleSubmit}>
+        <center>
+          <FormControl className={classes.formControl}>
+            <InputLabel id="demo-simple-select-label">Profile Icon</InputLabel>
+            <Select labelId="demo-simple-select-label" id="demo-simple-select">
+              <MenuItem value="sample1" className="menu-items">
+                Sample1
+              </MenuItem>
+              <MenuItem value="sample2">Sample2</MenuItem>
+              <MenuItem value="sample3">Sample 3</MenuItem>
+            </Select>
+          </FormControl>
+        </center>
+        <center>
+          <FormControl variant="outlined">
+            <InputLabel htmlFor="username">Username</InputLabel>
+            <OutlinedInput
+              id="username"
+              className="register-field"
+              type="text"
+              label="username"
+            />
+          </FormControl>
+        </center>
+        <br />
+        <center>
+          <FormControl variant="outlined">
+            <InputLabel htmlFor="component-outlined">Password</InputLabel>
+            <OutlinedInput
+              id="component-outlined"
+              className="register-field"
+              type="password"
+              label="password"
+            />
+          </FormControl>
+        </center>
+
+        <center>
+          <br />
+          <button id="register" type="submit">
+            SIGN UP!
+          </button>
+        </center>
+      </form>
+    </div>
+  );
+};
+
+export default Register;
